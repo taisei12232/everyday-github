e12f37a45beb43c0520a24c8c83b6427e60da64b
taisei12232 <taisei12232@gmail.com>
2022-07-01 11:59:59 +0900

    COMMIT

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import datetime
+import locale
+
+dt = datetime.datetime.today()
+print(dt.strftime('%b '))
+"""for i in range(365):
+    os.system(f'echo {i} > count.txt')
+    os.system('cat count.txt')"""
