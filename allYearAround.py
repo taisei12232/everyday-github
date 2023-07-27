9fa0730d37dda49072ce7401e15e9f9ba83c04b7
taisei12232 <taisei12232@gmail.com>
2023-07-27 13:00:11 +0900

    update

--- allYearAround.py
+++ allYearAround.py
@@ -2,6 +2,7 @@ import os
 import datetime
 import locale
 import uuid
+import subprocess
 
 dt = datetime.datetime.today()
 print(dt.strftime("%b %d %H:%M:%S %Y")+ " +0900")
@@ -9,7 +10,7 @@ for i in range(365):
     id = uuid.uuid4()
     pastdt = dt - datetime.timedelta(days=365-i)
     date = pastdt.strftime("%b %d 3:34:00 %Y")+ " +0900"
-    os.system(f'echo {id} > count.txt')
-    os.system('git add count.txt')
-    os.system(f'git commit --allow-empty -m "{id}" --date="{date}"')
-    os.system('git push origin master')
+    subprocess.run(f'echo {id} > count.txt', shell=True)
+    subprocess.run(f'git add count.txt', shell=True)
+    subprocess.run(f'git commit --allow-empty -m "{id}" --date="{date}"', shell=True)
+subprocess.run(f'git push origin master', shell=True)
